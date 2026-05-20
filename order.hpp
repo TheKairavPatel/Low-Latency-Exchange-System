fa78cddb772e3170310828a4f0d073009ea0d342
TheKairavPatel <kairavpatel13@gmail.com>
2026-05-20 15:24:35 -0500

    first stage

---
+++ order.hpp
@@ -0,0 +1,23 @@
+#include <cstdint>
+
+struct ClientOrder 
+{
+    uint32_t price;
+    uint32_t quantity;
+    uint16_t orderID;
+    uint8_t type; // 0 for buy, 1 for sell, 2 for cancel
+};
+
+struct EngineOrder {
+    uint32_t quantity;
+    uint16_t orderID;
+    uint8_t  levelNext;
+    uint8_t  levelPrev;
+};
+
+struct GlobalOrderInfo
+{
+    uint16_t priceLevel;
+    uint8_t posInArray;
+    uint8_t side; // 0 for buy, 1 for sell
+};
